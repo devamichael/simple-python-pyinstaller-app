66a6bbaa247c60817113a7dd726f68d69afe440e
devamichael <98259700+devamichael@users.noreply.github.com>
2022-01-23 17:31:25 +0200

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -2,11 +2,13 @@
 The 'calc' library contains the 'add2' function that takes 2 values and adds
 them together. If either value is a string (or both of them are) 'add2' ensures
 they are both strings, thereby resulting in a concatenated result.
-NOTE: If a value submitted to the 'add2' function is a float, it must be done so
-in quotes (i.e. as a string).
+NOTE: If a value submitted to the 'add2' function is a float, it must be done
+so in quotes (i.e. as a string).
 '''
 
-# If 'value' is not an integer, convert it to a float and failing that, a string.
+# If 'value' is not an integer, convert it to a float
+# and failing that, a string.
+
 def conv(value):
     try:
         return int(value)
@@ -17,6 +19,7 @@ def conv(value):
             return str(value)
 
 # The 'add2' function itself
+
 def add2(arg1, arg2):
     # Convert 'arg1' and 'arg2' to their appropriate types
     arg1conv = conv(arg1)
